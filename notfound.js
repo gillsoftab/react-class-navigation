123761be1ba2e6477b39c1e3ea739cde96f6df19
Niklas <49271685+gillsoftab@users.noreply.github.com>
2023-06-11 19:42:13 +0200

    Create notfound.js

---
+++ notfound.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import withRouter from './withRouter';
+
+class NotFoundPage extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: false
+    };
+  }
+
+  navigateTo(to = '/') {
+    this.props.navigate(to, { replace: true });
+  }
+
+  componentDidMount() {
+    
+  }
+
+  componentWillUnmount() {
+    
+  }
+
+  render() {
+    const { loading } = this.state;
+    const title = 'I am a title';
+    const text = 'Oops page not found!';
+    return (
+      <div>
+        <div style={{ textAlign: 'center' }}>{title}</div>
+        <div style={{ textAlign: 'center' }}>{text}</div>
+      </div>
+    );
+  }
+}
+
+NotFoundPage.propTypes = {
+  navigate: PropTypes.func,
+  params: PropTypes.object
+};
+
+export default withRouter(NotFoundPage);
