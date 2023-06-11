9f8f170e67daa36e00d3659e28d8c1f407d4d858
Niklas <49271685+gillsoftab@users.noreply.github.com>
2023-06-11 19:36:37 +0200

    Create about.js

---
+++ about.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import withRouter from './withRouter';
+
+class AboutPage extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: false
+    };
+  }
+  
+  navigateTo(to = '/') {
+    this.props.navigate(to, { replace: true });
+  }
+
+  componentDidMount() {
+    
+  }
+
+  componentWillUnmount() {
+    
+  }
+
+  render() {
+    const { loading } = this.state;
+    const title = 'I am a title';
+    return (
+      <div>
+        <div style={{ textAlign: 'center' }}>{title}</div>
+      </div>
+    );
+  }
+}
+
+AboutPage.propTypes = {
+  navigate: PropTypes.func,
+  params: PropTypes.object
+};
+
+export default withRouter(AboutPage);
